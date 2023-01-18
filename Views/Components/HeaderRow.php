bd48e7c47f896aecfebbc9f5c8a4f3b938058521
Jose Sissa <jsissame3484@universidadean.edu.co>
2023-01-17 23:03:48 -0500

    Cambiando los estilos del header

--- Views/Components/HeaderRow.php
+++ Views/Components/HeaderRow.php
@@ -9,10 +9,10 @@ class HeaderRow
         ?>
 		<header class="banner">
 			<div class="imagenes-logos">
-				<img src="Resources/img/logos/CCB.png" alt="Logo-">
-				<img src="Resources/img/logos/CCTN.jpg" alt="Logo-">
-				<img src="Resources/img/logos/PNUD.jpg" alt="Logo-">
-				<img src="Resources/img/logos/USAID.jpg" alt="Logo-">
+				<img src="Resources/img/logos/CCB.png" alt="Logo-ccb">
+				<img src="Resources/img/logos/CCTN.jpg" alt="Logo-cctn">
+				<img src="Resources/img/logos/PNUD.jpg" alt="Logo-pnud">
+				<img src="Resources/img/logos/5T.png" alt="Logo-5t">
 			</div>
 		</header>
 		<?php
